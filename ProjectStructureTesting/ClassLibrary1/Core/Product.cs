3edb55bd51fa0693092d17a76a8cd9b065c0ac7a
jqlee <jiachi.lee@gmail.com>
2014-09-18 21:25:23 +0800

    add class library 1

---
+++ ProjectStructureTesting/ClassLibrary1/Core/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Core {
+	/// <summary>
+	/// 產品
+	/// </summary>
+	public class Product {
+		/// <summary>
+		/// 識別碼
+		/// </summary>
+		public Guid Identity { get; set; }
+		/// <summary>
+		/// 名稱
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// 類別
+		/// </summary>
+		public Category ProductCategory { get; set; }
+		/// <summary>
+		/// 描述
+		/// </summary>
+		public string Description { get; set; }
+	}
+}
